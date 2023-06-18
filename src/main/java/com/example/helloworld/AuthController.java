f5d2ac0af0eb7e2156e63baa14b81f57cfdf2da1
PUSHRSP <pushrsp@gmail.com>
2023-06-18 22:32:35 +0900

    ADD: 레디스 활용 세션 공유

--- src/main/java/com/example/helloworld/AuthController.java
+++ src/main/java/com/example/helloworld/AuthController.java
@@ -5,23 +5,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 public class AuthController {
 
-    Map<String, String> sessionMap = new HashMap<>();
-
     @GetMapping("/login")
     public String login(HttpSession session, @RequestParam String name) {
-        sessionMap.put(session.getId(), name);
+        session.setAttribute("name", name);
 
         return "saved";
     }
 
     @GetMapping("/myName")
     public String myName(HttpSession session) {
-        return sessionMap.get(session.getId());
+        return (String) session.getAttribute("name");
     }
 }
