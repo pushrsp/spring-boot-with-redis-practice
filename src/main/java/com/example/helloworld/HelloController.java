38b4ada80178792d858ace6658b703e79a0c85c1
PUSHRSP <pushrsp@gmail.com>
2023-06-18 19:24:45 +0900

    ADD: redis 활용 get/set API 추가

--- src/main/java/com/example/helloworld/HelloController.java
+++ src/main/java/com/example/helloworld/HelloController.java
@@ -1,13 +1,39 @@
 package com.example.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    private final StringRedisTemplate redisTemplate;
+
+    @Autowired
+    public HelloController(StringRedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
     @GetMapping("/hello")
     public String hello() {
         return "hello world!";
+    }
+
+    @GetMapping("/setX")
+    public String setX(@RequestParam String name) {
+        ValueOperations<String, String> ops = redisTemplate.opsForValue();
+        ops.set("test", name);
+
+        return "saved";
+    }
+
+    @GetMapping("/getX")
+    public String getX() {
+        ValueOperations<String, String> ops = redisTemplate.opsForValue();
+
+        return ops.get("test");
     }
 }
