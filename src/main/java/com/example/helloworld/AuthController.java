7f66d38595fd5a14bc988ab389fa974873ed3768
PUSHRSP <pushrsp@gmail.com>
2023-06-18 22:21:11 +0900

    ADD: 간단한 인증 로직 구현

--- src/main/java/com/example/helloworld/AuthController.java
+++ src/main/java/com/example/helloworld/AuthController.java
@@ -1,4 +1,27 @@
 package com.example.helloworld;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 public class AuthController {
+
+    Map<String, String> sessionMap = new HashMap<>();
+
+    @GetMapping("/login")
+    public String login(HttpSession session, @RequestParam String name) {
+        sessionMap.put(session.getId(), name);
+
+        return "saved";
+    }
+
+    @GetMapping("/myName")
+    public String myName(HttpSession session) {
+        return sessionMap.get(session.getId());
+    }
 }
